665d0f73f6b83040f30dde16f56ccd2fa3282eb7
ashraf <ashraf.chzm@gmail.com>
2025-03-17 06:32:23 +0800

    feat(s): remove s

--- src/app/components/TweetForms.tsx
+++ src/app/components/TweetForms.tsx
@@ -27,7 +27,7 @@ export default function TweetForm({
     setError(null);
     
     try {
-      const response = await fetch('/api/tweets', {
+      const response = await fetch('/api/tweet', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
