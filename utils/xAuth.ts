ff63f20b98470488e81eee471815fd79349fa8ca
ashraf <ashraf.chzm@gmail.com>
2025-03-18 22:05:42 +0800

    feat(url): change URL

--- utils/xAuth.ts
+++ utils/xAuth.ts
@@ -225,7 +225,8 @@ console.log('x-- urls:', url);
     try {
       // Make the API request to post the tweet
       return this.makeAuthenticatedRequest(
-        'https://api.x.com/1.1/statuses/update.json',
+        // 'https://api.x.com/1.1/statuses/update.json',
+        'https://api.twitter.com/2/tweets',
         'POST',
         tweetData
       );
