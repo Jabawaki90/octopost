1078038385a8b9fcd9625088e63265e0bcc6fdca
ashraf <ashraf.chzm@gmail.com>
2025-03-21 06:09:59 +0800

    feat(cookies): remove delete cookies

--- src/app/api/auth/x/route.ts
+++ src/app/api/auth/x/route.ts
@@ -6,8 +6,6 @@ import { XAuth } from '../../../../../utils/xAuth';
 export async function GET() {
   try {
     const cookie = await cookies();
-    cookie.delete('x_access_token')
-    cookie.delete('x_access_token_secret')
     const xAuth = new XAuth({
       apiKey: process.env.CLIENT_API_KEY!,
       apiSecretKey: process.env.CLIENT_API_SECRET_KEY!,
